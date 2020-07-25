4c078abc4ca3757ecb9420657327a0ecb5964a6d
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-25 19:10:37 +0300

    mongo-db bağlantısı kuruldu

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDatabase = require('./helpers/database/connectDatabase');
 const routers = require("./routers/index");
 
 const app = express();
 
 dotenv.config({
     path: "./config/env/config.env"
-})
+});
+
+// MongoDB Connection
+connectDatabase();
+
 
 const PORT = process.env.PORT
 
