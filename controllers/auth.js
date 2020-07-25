26c0a61d3652489f9d4d64466fa91a0053976d28
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-25 22:36:15 +0300

    User kayıt işlemi uygulandı

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
-const register = (req,res,next) => {
+const User = require('../models/User');
+const register =async (req,res,next) => {
+
+    //Sample User Data
+    const name = "Serdar";
+    const email = "serdar@gmail.com";
+    const password = "123456";
+
+    //async await 
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: password
+    });
+
     res
     .status(200)
     .json({
-        success:true
+        success:true,
+        data: user
     });
 };
 
