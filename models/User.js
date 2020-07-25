eab3e9e144513cb234eb6c47e10facb52023338d
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-25 20:54:29 +0300

    User Schema'sı oluşturuldu

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"] // name alanı girilmediğinde verilecek mesajı tanımladık
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: [
+            true, "Please try different email" // email alanı veritabanında varsa verilecek mesajı tanımladık
+        ],
+        match: [
+            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, //internetten aldığımız hazır email regex'i
+            "Please provide a valid email" // email alanı email formatına uymuyorsa varsa verilecek mesajı tanımladık
+        ]
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user","admin"] 
+    },
+    password: {
+        type: String,
+        minlength: [6, "Please provide a password with min lenght 6"],
+        required: [true, "Please provide a password"],
+        select: false //Bu sayede user bilgisi çekildiği zaman password bilgisinin görünmemesi sağlanmış oldu.
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    title: {
+        type: String
+    },
+    about: {
+        type: String
+    },
+    place: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    profile_image: {
+        type: String,
+        default: "default.jpg"
+    },
+    blocked: {
+        type: Boolean,
+        default: false
+    }
+     
+});
+
+module.exports = mongoose.model("User", UserSchema);
