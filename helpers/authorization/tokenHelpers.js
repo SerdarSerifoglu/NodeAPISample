08086a5f7cc49c8dde2192aa31def3855c625246
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-26 17:05:46 +0300

    header'dan token kontrolü yapılması sağlandı.

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -24,9 +24,16 @@ const isTokenIncluded = (req) => {
     );
 };
 
+const getAccessTokenFromHeader = (req) => {
+    const authorization = req.headers.authorization;
+    
+    const access_token = authorization.split(" ")[1];
+    return access_token;
+}
 
 
 module.exports = {
     sendJwtToClient,
-    isTokenIncluded
-}
+    isTokenIncluded,
+    getAccessTokenFromHeader
+};
--- middlewares/authorization/auth.js
+++ middlewares/authorization/auth.js
@@ -1,14 +1,26 @@
 const CustomerError = require('../../helpers/error/CustomError');
 const jwt = require('jsonwebtoken');
-const { isTokenIncluded } = require("../../helpers/authorization/tokenHelpers");
+const { isTokenIncluded, getAccessTokenFromHeader } = require("../../helpers/authorization/tokenHelpers");
 
 const getAccessToRoute = (req,res,next) => {
+    const {JWT_SECRET_KEY} = process.env;
     if(!isTokenIncluded(req)){
         return next(
             new CustomerError("You are not authorized to access this route", 401)
         );
     };
+    const accessToken = getAccessTokenFromHeader(req);
+
+    jwt.verify(accessToken,JWT_SECRET_KEY, (err,decoded) => {
+        if(err){
+            return next(new CustomerError("You are not authorized to access this route", 401));
+        }
+        console.log(decoded);
+        next();
+    });
+
 };
+
 
 module.exports = {
     getAccessToRoute
