80e6ccb24536737308bdc418bc981890df9815d8
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-26 06:38:39 +0300

    Asnyc yapılardaki hataları yakalamak için try catch yerine kullanılacak modülün implementasyon örneği yapıldı

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,16 @@
 const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
-const register =async (req,res,next) => {
+const asyncErrorWrapper = require("express-async-handler");
+
+
+const register = asyncErrorWrapper(async (req,res,next) => {
 
     //Sample User Data
     const name = "Serdar22";
     const email = "serdar22@gmail.com";
     const password = "123456";
 
-    try{
+    
     //async await 
     const user = await User.create({
         name: name,
@@ -15,17 +18,12 @@ const register =async (req,res,next) => {
         password: password
     });
 
-    res
-    .status(200)
+    res.status(200)
     .json({
         success:true,
         data: user
     });
-    }catch(err){
-        return next(err);
-    }
-   
-};
+});
 
 const errorTest = (req,res,next) => {
     //Some Code
