4398f49b11c26852ddfe610df2292f9873762015
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-30 23:10:30 +0300

    static dosyalar eklenebilir hale geldi

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const connectDatabase = require('./helpers/database/connectDatabase');
 const customErrorHandler = require("./middlewares/errors/customErrorHandler");
 const routers = require("./routers/index");
+//path express içindeki bir paket
+const path = require('path');
 
 const app = express();
 
@@ -25,8 +27,9 @@ app.use("/api", routers);
 //Error Handler
 app.use(customErrorHandler);
 
-
-
+//Static Files
+app.use(express.static(path.join(__dirname, "public"))); // yaptığı iş dirname ile public ismini bağlamak
+console.log(path.join(__dirname, "public")); //örnek
 
 app.listen(PORT, function(){
     console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
