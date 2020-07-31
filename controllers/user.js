9e74a239d7f9554be668fa56a8391a4ce216f156
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-07-31 19:18:25 +0300

    user'ın veri tabanında varlık kontrolü middleware haline getirildi

--- controllers/user.js
+++ controllers/user.js
@@ -6,10 +6,6 @@ const asyncErrorWrapper = require("express-async-handler");
 const getSingleUser = asyncErrorWrapper(async (req,res,next) => {
     const {id} = req.params;
     const user = await User.findById(id);
-    
-    if(!user){
-        return next(new CustomError("There is no such user with that id", 400));
-    }
 
     return res.status(200)
     .json({
--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {getSingleUser} = require('../controllers/user');
+const {checkUserExist} = require('../middlewares/database/databaseErrorHelpers');
 const router = express.Router();
 
-router.get("/:id", getSingleUser);
+router.get("/:id",checkUserExist, getSingleUser);
 
 module.exports = router;
