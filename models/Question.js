981e3f2464a1a62a33407bc45d340ba688191387
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-08-01 20:54:37 +0300

    Answer modeli eklendi

--- models/Question.js
+++ models/Question.js
@@ -29,6 +29,10 @@ const QuestionSchema = new Schema({
     likes: [{
         type: mongoose.Schema.ObjectId,
         ref: "User"
+    }],
+    answers: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "Answer"
     }]
 });
 
