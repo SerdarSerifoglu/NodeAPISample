900ef566f8ef16b2afdd1c53c2944dc6c256e0b8
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-08-03 17:05:11 +0300

    Cevap güncelleme işlemi yapıldı

--- routers/answer.js
+++ routers/answer.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { merge } = require('./admin');
 const {getAccessToRoute} = require("../middlewares/authorization/auth");
 const {checkQuestionAndAnswerExist} = require("../middlewares/database/databaseErrorHelpers");
+const {getAnswerOwnerAccess} = require("../middlewares/authorization/auth");
 
 const {
     addNewAnswerToQuestion,
     getAllAnswersByQuestion,
-    getSingleAnswer
+    getSingleAnswer,
+    editAnswer
     } = require("../controllers/answer");
 
 //mergeParams: önceki router'daki parametrelerinde bu router'a geçirilmesini sağlar.
@@ -15,4 +17,5 @@ const router = express.Router({mergeParams: true});
 router.get("/", getAllAnswersByQuestion);
 router.get("/:answer_id", checkQuestionAndAnswerExist, getSingleAnswer);
 router.post("/",getAccessToRoute,addNewAnswerToQuestion);
+router.put("/:answer_id/edit", [checkQuestionAndAnswerExist, getAccessToRoute, getAnswerOwnerAccess], editAnswer);
 module.exports = router;
