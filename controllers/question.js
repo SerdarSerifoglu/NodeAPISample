4272812db1f09674023500b8ab7ab6d527ecad19
“SerdarSerifoglugit config --global user.name “SerdarSerifoglu <serdarserifoglu@gmail.com git config --global user.email serdarserifoglu@gmail.com>
2020-08-08 23:07:34 +0300

    sorular için sayfalama işlemleri uygulandı

--- controllers/question.js
+++ controllers/question.js
@@ -23,6 +23,29 @@ const getAllQuestions =asyncErrorWrapper(async (req,res,next) => {
     if(populate){
         query = query.populate(populateObject);
     }
+    //Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const pagination = {};
+    const total = await Question.countDocuments();
+
+    if(startIndex > 0){
+        pagination.previous = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+    if(endIndex < total){
+        pagination.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+    query = query.skip(startIndex).limit(limit);
 
     const questions = await query;
 
@@ -30,6 +53,8 @@ const getAllQuestions =asyncErrorWrapper(async (req,res,next) => {
     .json({
         success : true,
         message: "Get All Questions",
+        count: questions.length,
+        pagination: pagination,
         data: questions
     });
 });
